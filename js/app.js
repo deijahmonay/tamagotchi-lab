672aa8120a5d52a3eb62faf3f9ee6fdfaf753ea8
deijahmonay <deijah@shack15.com>
2024-08-13 14:31:48 -0700

    step 1 pass

--- js/app.js
+++ js/app.js
@@ -3,7 +3,14 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+// our global state object
+let state = {
+    boredem: 0,
+    hunger: 0,
+    sleepiness: 0,
+}
 
+let timer, gameOver; // this will assign the timer & gameOver a value of undefined
 
 
 /*------------------------ Cached Element References ------------------------*/
